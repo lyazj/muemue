c5b20b0aa4648b348a913dc5952620c9ef188b26
lyazj <seeson@pku.edu.cn>
2024-11-01 21:37:45 +0800

    Update

---
+++ make_muemue_examples_160GeV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import multiprocessing
+from make_muemue_point import muemue_card
+
+pool = multiprocessing.Pool(6)
+
+args = []
+muon_energy = 160.0  # GeV
+for min_lepton_pt in np.array([  # GeV
+    0.001000,
+    0.010000,
+    0.100000,
+]):
+    for min_lepton_energy in np.array([  # GeV
+        #0.001000,
+        #0.010000,
+        0.100000,
+    ]):
+        if min_lepton_energy < min_lepton_pt: continue
+        repeat = 1
+        for r in range(repeat):
+            args.append({
+                'workdir': f'muemue_example_{muon_energy}GeV_pT_{min_lepton_pt:.0e}GeV_E_{min_lepton_energy:.0e}GeV_{r}',
+                'nevent': 100000, 'seed': r, 'electron_energy': 0.000511,  # GeV
+                'muon_energy': muon_energy, 'min_lepton_pt': min_lepton_pt, 'min_lepton_energy': min_lepton_energy,
+            })
+pool.map(muemue_card.run, args)
--- plot_muemue_kinematics.py
+++ plot_muemue_kinematics.py
@@ -21,11 +21,11 @@ fields = [
 params = []
 data = {'xs': {}, 'E': {}, 'theta': {} }  # [key][(muon_energy, min_lepton_pt)]
 
-for muemue_dirname in sorted(glob.glob('muemue_example_*GeV_*GeV/')):
-    r = re.search(r'^muemue_example_([0-9.eE+-]+)GeV_([0-9.eE+-]+)GeV/$', muemue_dirname)
+for muemue_dirname in sorted(glob.glob('muemue_example_*GeV_*GeV_0/')):
+    r = re.search(r'^muemue_example_([0-9.eE+-]+)GeV_pT_([0-9.eE+-]+)GeV_E_([0-9.eE+-]+)GeV_0/$', muemue_dirname)
     if not r: continue
-    muon_energy, min_lepton_pt = map(float, r.groups())
-    params.append((muon_energy, min_lepton_pt))
+    muon_energy, min_lepton_pt, min_lepton_energy = map(float, r.groups())
+    params.append((muon_energy, min_lepton_pt, min_lepton_energy))
 
     # Load events.
     rootpath = os.path.join(muemue_dirname, 'Events', 'run_01', 'unweighted_events.root')
@@ -50,35 +50,41 @@ for muemue_dirname in sorted(glob.glob('muemue_example_*GeV_*GeV/')):
     dp = p / np.sqrt(np.sum(p*p, axis=2, keepdims=True))
     theta = np.arccos(np.minimum(1.0, dp[:,:,2]))
 
-    data['xs'][(muon_energy, min_lepton_pt)] = ak.mean(tree['Event.Weight'])
-    data['E'][(muon_energy, min_lepton_pt)] = E
-    data['theta'][(muon_energy, min_lepton_pt)] = theta
+    data['xs'][(muon_energy, min_lepton_pt, min_lepton_energy)] = ak.mean(tree['Event.Weight'])
+    data['E'][(muon_energy, min_lepton_pt, min_lepton_energy)] = E
+    data['theta'][(muon_energy, min_lepton_pt, min_lepton_energy)] = theta
 
-for key, data_value in data.items():
-    plt.figure(dpi=300)
-    if key == 'xs':
-        for (muon_energy, min_lepton_pt), value in sorted(data_value.items()):
-            print(key, muon_energy, min_lepton_pt)
-            label = f'$E_\\mu = {muon_energy}\\ \\mathrm{{GeV}},\\ p_{{\\mathrm{{T, min}}}} = \\text{{{min_lepton_pt:.0e}}}\\ \\mathrm{{GeV}}$'
-            plt.scatter(0, value, label=label)
-        plt.xticks()
-        plt.ylabel(f'Cross section [pb]')
-        plt.yscale('log')
-    else:
-        for (muon_energy, min_lepton_pt), value in sorted(data_value.items()):
-            print(key, muon_energy, min_lepton_pt)
-            label = f'$E_\\mu = {muon_energy}\\ \\mathrm{{GeV}},\\ p_{{\\mathrm{{T, min}}}} = \\text{{{min_lepton_pt:.0e}}}\\ \\mathrm{{GeV}}$'
-            plt.scatter(value[:,1], value[:,0], label=label)
-        plt.xlabel(f'electron {key}')
-        plt.ylabel(f'muon {key}')
-    plt.legend(loc='upper right')
-    plt.grid()
-    plt.tight_layout()
-    plt.savefig(f'muemue_{key}.png')
-    plt.close()
+for muon_energy_expected in sorted(set(param[0] for param in params)):
+    for min_lepton_energy_expected in sorted(set(param[2] for param in params)):
+        for key, data_value in data.items():
+            plt.figure(dpi=300)
+            if key == 'xs':
+                for (muon_energy, min_lepton_pt, min_lepton_energy), value in sorted(data_value.items()):
+                    if muon_energy != muon_energy_expected: continue
+                    if min_lepton_energy != min_lepton_energy_expected: continue
+                    print(key, muon_energy, min_lepton_pt)
+                    label = f'$E_\\mu = {muon_energy}\\ \\mathrm{{GeV}},\\ p_{{\\mathrm{{T, min}}}} = \\text{{{min_lepton_pt:.0e}}}\\ \\mathrm{{GeV}}$'
+                    plt.scatter(0, value, label=label)
+                plt.xticks()
+                plt.ylabel(f'Cross section [pb]')
+                plt.yscale('log')
+            else:
+                for (muon_energy, min_lepton_pt, min_lepton_energy), value in sorted(data_value.items()):
+                    if muon_energy != muon_energy_expected: continue
+                    if min_lepton_energy != min_lepton_energy_expected: continue
+                    print(key, muon_energy, min_lepton_pt, min_lepton_energy)
+                    label = f'$E_\\mu = {muon_energy}\\ \\mathrm{{GeV}},\\ p_{{\\mathrm{{T, min}}}} = \\text{{{min_lepton_pt:.0e}}}\\ \\mathrm{{GeV}},\\ E_{{\\mathrm{{min}}}} = \\text{{{min_lepton_energy:.0e}}}\\ \\mathrm{{GeV}}$'
+                    plt.scatter(value[:,1], value[:,0], label=label)
+                plt.xlabel(f'electron {key}')
+                plt.ylabel(f'muon {key}')
+            plt.legend(loc='upper right')
+            plt.grid()
+            plt.tight_layout()
+            plt.savefig(f'muemue_{muon_energy_expected}GeV_{key}_E_{min_lepton_energy_expected:.0e}GeV.png')
+            plt.close()
 
 for param in params:
-    muon_energy, min_lepton_pt = param
-    with open(f'muemue_example_{muon_energy}GeV_{min_lepton_pt:.0e}GeV.txt', 'w') as file:
+    muon_energy, min_lepton_pt, min_lepton_energy = param
+    with open(f'muemue_example_{muon_energy}GeV_pT_{min_lepton_pt:.0e}GeV_E_{min_lepton_energy:.0e}GeV.txt', 'w') as file:
         for theta, E in zip(data['theta'][param], data['E'][param]):
             print(' '.join(['%.18e'] * 4) % (*theta, *E), file=file)
