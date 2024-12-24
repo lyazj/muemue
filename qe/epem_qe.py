419ac824d19b081a2ff05f69e630298185eb3c44
lyazj <seeson@pku.edu.cn>
2024-12-24 19:59:18 +0800

    Update epem_qe.py

--- qe/epem_qe.py
+++ qe/epem_qe.py
@@ -5,7 +5,7 @@ from matplotlib import colors as mcolors
 
 # Optional maximum number of events to process.
 NEVENT_MAX = None
-COM_FRAME = True
+COM_FRAME = False
 STACK = True
 BELL = True
 
@@ -210,6 +210,13 @@ for path in [  # sample files
     eigenvalues = np.sort(np.sqrt(eigenvalues), axis=1)
     concurrence = eigenvalues[:,-1] - np.sum(eigenvalues[:,:-1], axis=1)
     concurrence = np.maximum(concurrence, 0)
+    print('Positron energy:', positron_energy)
+    print('Max concurrence:', np.max(concurrence))
+    print('Efficiency:', np.mean(concurrence > 0))
+    print('Min theta_p:', np.min(theta_p[concurrence > 0]))
+    print('Min theta_e:', np.min(theta_e[concurrence > 0]))
+    print('Min E_p:', np.min(E_p[concurrence > 0]))
+    print('Min E_e:', np.min(E_e[concurrence > 0]))
 
     if BELL:
         corr = np.sum(sigma_kron.reshape(1, 3, 3, 16) * rho.transpose(0, 2, 1).reshape(-1, 1, 1, 16), axis=3)
@@ -219,6 +226,7 @@ for path in [  # sample files
         eigenvalues = np.sort(np.sqrt(eigenvalues), axis=1)
         CHSH = 2 * np.sqrt(np.sum(eigenvalues[:,-2:], axis=1))
         print('Efficiency:', np.mean(CHSH[concurrence > 0] > 2))
+        print('Max CHSH:', np.max(CHSH))
 
     # Plot concurrence as a function of theta_p and theta_e.
     data = np.array([theta_p, theta_e, CHSH if BELL else concurrence]).T
