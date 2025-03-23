6ca189a604e3aebc9604d4a3ccac83317c6868b9
lyazj <seeson@pku.edu.cn>
2025-03-23 14:27:01 +0800

    Add 2 GeV and 8 GeV scripts

---
+++ make_muemue_examples_2GeV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import multiprocessing
+from make_muemue_point import muemue_card
+
+pool = multiprocessing.Pool(6)
+
+args = []
+muon_energy = 2.0  # GeV
+for min_lepton_pt in np.array([  # GeV
+    0.0,
+]):
+    for min_lepton_eta in np.array([
+        #2.85,
+        #2.86,
+        #2.87,
+        2.88,
+        #2.89,
+    ]):
+        repeat = 1
+        for r in range(repeat):
+            args.append({
+                'workdir': f'muemue_example_{muon_energy}GeV_pT_{min_lepton_pt:.2e}GeV_eta_{min_lepton_eta:.2e}_{r}',
+                'nevent': 1000000, 'seed': r, 'electron_energy': 0.000511,  # GeV
+                'muon_energy': muon_energy,
+                'min_lepton_pt': min_lepton_pt, 'min_lepton_com_energy': -1.0, 'min_lepton_eta': min_lepton_eta,
+            })
+pool.map(muemue_card.run, args)
